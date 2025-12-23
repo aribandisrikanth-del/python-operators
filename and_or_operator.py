c6d6bf7de1edf542982363573c520638a9352e22
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2025-12-22 20:37:03 -0500

    Add files via upload

---
+++ and_or_operator.py
@@ -0,0 +1,11 @@
+a=1
+b=-1
+c=0
+if a and b and c:
+    print("all numbers have boolean value as true")
+else:
+    print("at least 1 number has boolean value as false")
+if a or b or c:
+    print("at least 1 number has a boolean value of true")
+else:
+    print("no numbers have the boolean value as true")
