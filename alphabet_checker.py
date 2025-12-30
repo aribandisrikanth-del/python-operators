83f4daa7e7c5ea314f91191de9df5bbccb22a7ef
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2025-12-29 19:35:25 -0500

    Add files via upload

---
+++ alphabet_checker.py
@@ -0,0 +1,55 @@
+check=input("type a alphabet or a non alphabet to check ")
+if check=="a":
+    print("your input was a alphabet")
+elif check=="b":
+    print("your input was a alphabet")
+elif check=="c":
+    print("your input was a alphabet")
+elif check=="d":
+    print("your input was a alphabet")
+elif check=="e":
+    print("your input was a alphabet")
+elif check=="f":
+    print("your input was a alphabet")
+elif check=="g":
+    print("your input was a alphabet")
+elif check=="h":
+    print("your input was a alphabet")
+elif check=="i":
+    print("your input was a alphabet")
+elif check=="j":
+    print("your input was a alphabet")
+elif check=="k":
+    print("your input was a alphabet")
+elif check=="l":
+    print("your input was a alphabet")
+elif check=="m":
+    print("your input was a alphabet")
+elif check=="n":
+    print("your input was a alphabet")
+elif check=="o":
+    print("your input was a alphabet")
+elif check=="p":
+    print("your input was a alphabet")
+elif check=="q":
+    print("your input was a alphabet")
+elif check=="r":
+    print("your input was a alphabet")
+elif check=="s":
+    print("your input was a alphabet")
+elif check=="t":
+    print("your input was a alphabet")
+elif check=="u":
+    print("your input was a alphabet")
+elif check=="v":
+    print("your input was a alphabet")
+elif check=="w":
+    print("your input was a alphabet")
+elif check=="x":
+    print("your input was a alphabet")
+elif check=="y":
+    print("your input was a alphabet")
+elif check=="z":
+    print("your input was a alphabet")
+else:
+    print("your input was not a alphabet")
